ff85eeb9a2e9f692cff374ffb55a3007ab0ccd64
Nahalius <ivan.v.petkov@gmail.com>
2017-02-18 08:30:23 +0200

    intro methods 9ji

---
+++ DataTypes/Methods/Methods/ConsoleApplication1/MethodsShort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MethodsShort
+    {
+        static void Main(string[] args)
+        {
+            InputData();
+            OutputData();
+        }
+
+
+        static void InputData()
+        {
+            Console.ReadLine();
+        }
+        static void OutputData()
+        {
+            Console.WriteLine();
+            
+        }
+
+    }
+}
