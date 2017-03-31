b4357df0c511c8b3ddd245e229268ebe9cfd3692
Nahalius <ivan.v.petkov@gmail.com>
2017-03-31 11:32:32 +0300

    added to methods my friend *9

--- DataTypes/Methods/Methods/ConsoleApplication1/MethodsShort.cs
+++ DataTypes/Methods/Methods/ConsoleApplication1/MethodsShort.cs
@@ -10,29 +10,42 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var number = 5;
-            string text = "some text"; //String.Format("Hello {0}",text) - for display
-            InputData();
+            Console.WriteLine("Write a number between 0 to +21");
+            var number = int.Parse(Console.ReadLine());
+            string text = "your number"; //String.Format("Hello {0}",text) - for display
+            
             OutputData(number); //With number
-            IfStatement();
+            IfStatement(number);
+            ForLoop(number,text);
         }
 
-        static void InputData()
+        static void ForLoop(int number, string text)
         {
-
-            Console.ReadLine();
+            for (int i = 1; i < number; i++) 
+            { 
+                Console.Write("This text is {0} minus one times your {1} " ,number, text);
+                Console.WriteLine();
+            }
         }
+
         static void OutputData(int number)
         {
-            Console.WriteLine(number);
-            
+            Console.WriteLine("Your number is " + number);   
         }
-        static void IfStatement()
+        static void IfStatement(int number)
         {
-            
-         
+             if (number > 0)  
+                {  
+                 Console.WriteLine("Positive");  
+                }  
+             else if (number < 0)
+                { 
+                 Console.WriteLine("Negative"); 
+                }
+             else 
+                { 
+                 Console.WriteLine("Zero"); 
+                } 
         }
-
-
     }
 }
